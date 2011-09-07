ab0def0c0b684a7c07bdbc54e9064abc926cbe41
Jesper Sjövall <jesper.sjovall@gmail.com>
2011-09-07 11:37:54 +0000

    Try to get Test Unit to work

---
+++ test/se/chalmers/snake/gameengine/PlayerBodyTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.snake.gameengine;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Figaro
+ */
+public class PlayerBodyTest {
+	
+	public PlayerBodyTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Test
+	public void testSomeMethod() {
+		System.out.println("Hello World");
+	}
+}
