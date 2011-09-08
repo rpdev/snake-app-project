84eb8f6bf47fdc41cfc2a232f4e5e2892ddda7ce
rpdev <>
2011-09-08 08:20:42 +0000

    Interface of the level map database

---
+++ src/se/chalmers/snake/interfaces/LevelDatabaseIC.java
@@ -0,0 +1,33 @@
+package se.chalmers.snake.interfaces;
+
+public interface LevelDatabaseIC {
+	
+	/**
+	 * Return a level map from the database based on the level
+	 * of that game map.  
+	 * @param level
+	 * @return
+	 */
+	public LevelIC getByLevel(int level);
+	
+	/**
+	 * Return level map form the database based on the name of
+	 * the level.
+	 * @param name
+	 * @return
+	 */
+	public LevelIC getByName(String name);
+	
+	/**
+	 * Return an array with the name of levels in the database.
+	 * @return
+	 */
+	public String[] getLevelListByName();
+	
+	/**
+	 * Return an array with the level of the level maps in the database,
+	 * from easiest to hardest.
+	 * @return
+	 */
+	public int[] getLevelListByLevel();
+}
