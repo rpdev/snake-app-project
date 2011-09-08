fe1989a3862fd43cdec8b3aa4668cced895715fe
rickard.persson.87 <rickard.persson.87@2e1859c2-e961-2241-5cca-a9a602cebfbf>
2011-09-08 07:17:42 +0000

    level

---
+++ trunk/src/se/chalmers/snake/leveldatabase/Level.java
@@ -0,0 +1,71 @@
+package se.chalmers.snake.leveldatabase;
+
+import java.util.List;
+
+import se.chalmers.snake.interfaces.LevelIC;
+import se.chalmers.snake.interfaces.util.REPoint;
+import se.chalmers.snake.interfaces.util.XYPoint;
+
+public class Level implements LevelIC {
+
+	@Override
+	public int getSpeed(int collectNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpeed(List<Integer> collectTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int hasRachGoal(int collectNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int hasRachGoal(List<Integer> collectTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getItemsCount(int collectNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getItemsCount(List<Integer> collectTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAddBodySegments(int collectNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAddBodySegments(List<Integer> collectTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<REPoint> getStaticElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public XYPoint getGameFiledSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
