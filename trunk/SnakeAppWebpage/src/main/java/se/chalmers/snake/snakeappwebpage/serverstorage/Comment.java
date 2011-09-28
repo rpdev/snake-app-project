fa6acc6007e456bd59306244381e04ecf7ab826d
rickard.persson.87@gmail.com <rickard.persson.87@gmail.com@2e1859c2-e961-2241-5cca-a9a602cebfbf>
2011-09-28 17:13:12 +0000

    rewritten many classes while trying to fix exceptions while building, pom.xml ...

---
+++ trunk/SnakeAppWebpage/src/main/java/se/chalmers/snake/snakeappwebpage/serverstorage/Comment.java
@@ -0,0 +1,169 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.snake.snakeappwebpage.serverstorage;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.Transient;
+
+@Entity
+public class Comment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Transient
+    private static final int COMMENT_MIN_LENGTH = 3;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String commentText;
+    @Column(nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date commentMade;
+    @Column
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date commentLatestEdit = null;
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private SnakeMap map;
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private UserAcc user;
+
+    public Comment() {
+    }
+
+    public Comment(String commentText, SnakeMap map, UserAcc user) {
+	if (map == null || commentText == null || commentText.trim().length() < COMMENT_MIN_LENGTH) {
+	    throw new IllegalArgumentException("Invalid arguments, "
+		    + "commentText:" + commentText
+		    + " SnakeMap:" + map
+		    + "User:" + user);
+	}
+	this.commentText = commentText;
+	commentMade = new Date();
+	this.map = map;
+	this.user = user;
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Getters & setters">
+    /**
+     * @return the id
+     */
+    public Long getId() {
+	return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    /**
+     * @return the commentText
+     */
+    public String getCommentText() {
+	return commentText;
+    }
+
+    /**
+     * @param commentText the commentText to set
+     */
+    public void setCommentText(String commentText) {
+	this.commentText = commentText;
+    }
+
+    /**
+     * @return the commentMade
+     */
+    public Date getCommentMade() {
+	return commentMade;
+    }
+
+    /**
+     * @param commentMade the commentMade to set
+     */
+    public void setCommentMade(Date commentMade) {
+	this.commentMade = commentMade;
+    }
+
+    /**
+     * @return the commentLatestEdit
+     */
+    public Date getCommentLatestEdit() {
+	return commentLatestEdit;
+    }
+
+    /**
+     * @param commentLatestEdit the commentLatestEdit to set
+     */
+    public void setCommentLatestEdit(Date commentLatestEdit) {
+	this.commentLatestEdit = commentLatestEdit;
+    }
+
+    /**
+     * @return the map
+     */
+    public SnakeMap getMap() {
+	return map;
+    }
+
+    /**
+     * @param map the map to set
+     */
+    public void setMap(SnakeMap map) {
+	this.map = map;
+    }
+
+    /**
+     * @return the user
+     */
+    public UserAcc getUser() {
+	return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(UserAcc user) {
+	this.user = user;
+    }
+    //</editor-fold>
+
+    @Override
+    public int hashCode() {
+	int hash = 0;
+	hash += (getId() != null ? getId().hashCode() : 0);
+	return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+	// TODO: Warning - this method won't work in the case the id fields are not set
+	if (!(object instanceof Comment)) {
+	    return false;
+	}
+	Comment other = (Comment) object;
+	if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.id.equals(other.id))) {
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "se.chalmers.snake.snakeappwebpage.serverstorage.Comment[ id=" + getId() + " ]";
+    }
+}
--- trunk/SnakeAppWebpage/src/main/java/se/chalmers/snake/snakeappwebpage/serverstorage/SnakeMap.java
+++ trunk/SnakeAppWebpage/src/main/java/se/chalmers/snake/snakeappwebpage/serverstorage/SnakeMap.java
@@ -1,71 +1,187 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package se.chalmers.snake.snakeappwebpage.serverstorage;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityManager;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import se.chalmers.snake.snakeappwebpage.serverstorage.Database.STATUS;
 
-/**
- *
- */
 @Entity
-public class SnakeMap  extends SelfPersistence implements Serializable{
-	private static final long serialVersionUID = -1792642630083319368L;
-	
-	
-	
-	
-	@Id
-   @GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+public class SnakeMap implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-	public long getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @JoinColumn(nullable=false)
+    @OneToOne
+    private UserAcc userName;
+    @Enumerated(EnumType.STRING)
+    private STATUS status;
+    @OneToMany(cascade= CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<Comment>();
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    @Column(nullable=false, updatable=false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date creationDate;
 
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (int) id;
-		return hash;
-	}
+    @Column
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date publicedDate;
 
-	@Override
-	public boolean equals(Object object) {
-		// TODO: Warning - this method won't work in the case the id fields are not set
-		if (!(object instanceof SnakeMap)) {
-			return false;
-		}
-		SnakeMap other = (SnakeMap) object;
-		if (this.id != other.id) {
-			return false;
-		}
-		return true;
-	}
+    @Column
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date editedDate;
 
-	@Override
-	public String toString() {
-		return "se.chalmers.snake.snakeappwebpage.serverstorage.SnakeMap[ id=" + id + " ]";
-	}
+    public SnakeMap(){}
 
-	@Override
-	SelfPersistence trackPersistence(EntityManager entityManager) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    public SnakeMap(UserAcc user){
+	this.userName = user;
+	creationDate = new Date();
+	status = STATUS.CREATED;
+    }
 
-	@Override
-	boolean trackDestroy(EntityManager entityManager, SelfPersistence removeObj) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+    //<editor-fold defaultstate="collapsed" desc="Getters & setters">
+    /**
+     * @return the id
+     */
+    public Long getId() {
+	return id;
+    }
 
-	void override(SnakeMap map) {
-		throw new UnsupportedOperationException("Not yet implemented");
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    /**
+     * @return the userName
+     */
+    public UserAcc getUserName() {
+	return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(UserAcc userName) {
+	this.userName = userName;
+    }
+
+    /**
+     * @return the status
+     */
+    public STATUS getStatus() {
+	return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(STATUS status) {
+	this.status = status;
+    }
+
+    /**
+     * @return the comments
+     */
+    public List<Comment> getComments() {
+	return comments;
+    }
+
+    /**
+     * @param comments the comments to set
+     */
+    public void setComments(List<Comment> comments) {
+	this.comments = comments;
+    }
+
+    /**
+     * @return the creationDate
+     */
+    public Date getCreationDate() {
+	return creationDate;
+    }
+
+    /**
+     * @param creationDate the creationDate to set
+     */
+    public void setCreationDate(Date creationDate) {
+	this.creationDate = creationDate;
+    }
+
+    /**
+     * @return the publicedDate
+     */
+    public Date getPublicedDate() {
+	return publicedDate;
+    }
+
+    /**
+     * @param publicedDate the publicedDate to set
+     */
+    public void setPublicedDate(Date publicedDate) {
+	this.publicedDate = publicedDate;
+    }
+
+    /**
+     * @return the editedDate
+     */
+    public Date getEditedDate() {
+	return editedDate;
+    }
+
+    /**
+     * @param editedDate the editedDate to set
+     */
+    public void setEditedDate(Date editedDate) {
+	this.editedDate = editedDate;
+    }
+    //</editor-fold>
+
+    @Override
+    public int hashCode() {
+	int hash = 0;
+	hash += (getId() != null ? getId().hashCode() : 0);
+	return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+	// TODO: Warning - this method won't work in the case the id fields are not set
+	if (!(object instanceof SnakeMap)) {
+	    return false;
 	}
-	
+	SnakeMap other = (SnakeMap) object;
+	if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.id.equals(other.id))) {
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "se.chalmers.snake.snakeappwebpage.serverstorage.SnakeMap[ id=" + getId() + " ]";
+    }
+
+
+
 }
