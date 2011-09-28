f5932ed222d47520a29bb83b60988158ea3b161a
jesper.sjovall <jesper.sjovall@2e1859c2-e961-2241-5cca-a9a602cebfbf>
2011-09-28 13:22:41 +0000

    Update Files,

--- trunk/SnakeApp/src/se/chalmers/snake/StartActivity.java
+++ trunk/SnakeApp/src/se/chalmers/snake/StartActivity.java
@@ -2,16 +2,10 @@ package se.chalmers.snake;
 
 import android.app.Activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.SensorManager;
-import se.chalmers.snake.gameengine.TestGameEngine;
-import se.chalmers.snake.interfaces.util.XYPoint;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.FrameLayout;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
