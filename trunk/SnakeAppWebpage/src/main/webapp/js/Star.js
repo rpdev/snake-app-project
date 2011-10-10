d07d0a5f8867e8a9f3f093a08a8a044c616516df
levi.799@gmail.com <levi.799@gmail.com@2e1859c2-e961-2241-5cca-a9a602cebfbf>
2011-10-10 01:50:49 +0000

    test

--- trunk/SnakeAppWebpage/src/main/webapp/js/Star.js
+++ trunk/SnakeAppWebpage/src/main/webapp/js/Star.js
@@ -1,54 +1,10 @@
 $(document).ready(function(){
-    // Try div.hover or div#divMain if not working 
-    $("#star1").hover(
-    function() {
-        setStars(1)            
-    }, 
-    function() {
-        clearStars();
-    }),
-    $("#star2").hover(
-    function() {
-        setStars(2)            
-    }, 
-    function() {
-        clearStars();
-    }),
-    $("#star3").hover(
-    function() {
-        setStars(3)            
-    }, 
-    function() {
-        clearStars();
-    }),
-    $("#star4").hover(
-    function() {
-        setStars(4)            
-    }, 
-    function() {
-        clearStars();
-    }),
-    $("#star5").hover(
-    function() {
-        setStars(5)            
-    }, 
-    function() {
-        clearStars();
-    })
+
+    function clearStars(){
+        for(k = 1; k <= 5; k++){
+            var src = "img/star_gray.png";
+            var namn = "#star"+k;
+            $(namn).attr("src", src);
+        }
+    }
 })
-
-function setStars(i){
-    for(k = 1; k <= i; k++){
-        var src = "img/star_yellow.png";
-        var namn = "#star"+k;
-        $(namn).attr("src", src);
-    }
-}
-
-function clearStars(){
-    for(k = 1; k <= 5; k++){
-        var src = "img/star_gray.png";
-        var namn = "#star"+k;
-        $(namn).attr("src", src);
-    }
-}
