f25ad552cf2760d852c0ce8bd5f2c13b3f515dcf
Jesper Sjövall <jesper.sjovall@gmail.com>
2011-10-14 09:16:42 +0000

    Remove you lose, that never will be show

--- SnakeApp/src/se/chalmers/snake/HighscoreActivity.java
+++ SnakeApp/src/se/chalmers/snake/HighscoreActivity.java
@@ -13,94 +13,73 @@ public class HighscoreActivity extends Activity {
 
 	private HighscoreDatabaseIC highscoreDatabase;
 	private int points;
-	
 	private Button skipButton;
 	private Button submitButton;
 	private TextView youMadeIt;
-	private TextView youDidntMakeIt;
 	private EditText inputName;
-	
 	private View.OnClickListener skipListener = new View.OnClickListener() {
+
 		@Override
 		public void onClick(View v) {
 			HighscoreActivity.this.finish();
 		}
 	};
-	
 	private View.OnClickListener submitListener = new View.OnClickListener() {
+
 		@Override
 		public void onClick(View v) {
-			String name = HighscoreActivity.this.inputName.getText().toString();
-			if(HighscoreActivity.this.savePoints(name,points)) {
+			String name = HighscoreActivity.this.inputName.getText().toString().trim();
+			if (HighscoreActivity.this.savePoints(name, HighscoreActivity.this.points)) {
 				HighscoreActivity.this.finish();
 			}
 		}
 	};
-	
-	
-	
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		this.setContentView(R.layout.highscore_layout);
-		
+
 		Bundle extras = this.getIntent().getExtras();
-		if(extras != null){
-			points = extras.getInt("points");
+		if (extras != null) {
+			this.points = extras.getInt("points");
 		}
-		
+
 		this.setObjects();
 		this.highscoreDatabase = ControlResources.get().getHighscoreDatabase();
-		if(checkIfEnoughPoints()){
+		if (checkIfEnoughPoints()) {
 			showYouMadeItMenu();
 		} else {
-			showSorryMenu();
+			this.finish();
 		}
-		
+
 	}
-	
-	private void setObjects(){
+
+	private void setObjects() {
 		//Set up buttons
-		this.skipButton = (Button)findViewById(R.id.skip_button);
+		this.skipButton = (Button) findViewById(R.id.skip_button);
 		this.skipButton.setOnClickListener(skipListener);
-		this.submitButton = (Button)findViewById(R.id.submit_button);
+		this.submitButton = (Button) findViewById(R.id.submit_button);
 		this.submitButton.setOnClickListener(submitListener);
 		//Set up text views
-		this.youMadeIt = (TextView)findViewById(R.id.you_made_it_text);
-		this.youDidntMakeIt = (TextView)findViewById(R.id.you_didnt_make_it_text);
+		this.youMadeIt = (TextView) findViewById(R.id.you_made_it_text);
 		//Set up edittext
-		this.inputName = (EditText)findViewById(R.id.input_edittext);
+		this.inputName = (EditText) findViewById(R.id.input_edittext);
 	}
-	
-	private boolean checkIfEnoughPoints(){
+
+	private boolean checkIfEnoughPoints() {
 		return highscoreDatabase.checkIfEnoughPoints(points);
 	}
-	
-	private void showYouMadeItMenu(){
-		//These elements should be visible
+
+	private void showYouMadeItMenu() {
 		this.youMadeIt.setVisibility(View.VISIBLE);
 		this.skipButton.setVisibility(View.VISIBLE);
 		this.submitButton.setVisibility(View.VISIBLE);
 		this.inputName.setVisibility(View.VISIBLE);
-		//These elements shouldn't
-		this.youDidntMakeIt.setVisibility(View.GONE);
 	}
-	
-	private void showSorryMenu(){
-		//These elements should be visible
-		this.youDidntMakeIt.setVisibility(View.VISIBLE);
-		this.skipButton.setVisibility(View.VISIBLE);
-		//skipbutton should be renamed
-//		this.skipButton.setText("Exit");
-		//These elements shouldn't
-		this.inputName.setVisibility(View.GONE);
-		this.youMadeIt.setVisibility(View.GONE);
-		this.submitButton.setVisibility(View.GONE);
-	}
-	
-	private boolean  savePoints(String name, int points){
+
+	private boolean savePoints(String name, int points) {
 		this.highscoreDatabase.addPlayerToHighscore(name, points);
 		return this.highscoreDatabase.saveHighscore();
 	}
-
 }
